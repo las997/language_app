ec5b90320276974ae8c45851490b57eb52dbb2f2
Courtney <manbeckcourtney@gmail.com>
2015-07-17 14:52:30 +0000

    here we go

--- application_controller.rb
+++ application_controller.rb
@@ -6,11 +6,12 @@ require_relative './models/noun_generator.rb'
 
 class MyApp < Sinatra::Base
 
+  
   get '/' do
     erb :index
   end
 
-  get '/results' do
+  get '/results.erb' do
     erb :results
   end
   
@@ -24,11 +25,26 @@ class MyApp < Sinatra::Base
       
   get '/noun1_results.erb' do
     @noun = Noun_Generator.new
+    noungenerator = Noun_Generator.new
+    rand_noun_key = noungenerator.rand_noun
+    rand_ending_key = noungenerator.rand_ending
+    @word = noungenerator.return_noun(rand_noun_key).to_s + noungenerator.return_ending(rand_ending_key).to_s
+    
     erb :noun1_results
+  end
+  
+  post '/noun1answer.erb' do
+     noun_input = #params[:text]
+     ending_input = #params[:dropdown]
+     noungenerator.read_compare_noun(noun_input, rand_noun_key)
   end
   
   get '/jnoun_results.erb' do
     erb :jnoun_results
+  end
+  
+  get '/action_page.erb' do
+    erb :action_page
   end
   
 end
